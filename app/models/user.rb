7ae773cfb4841f97f96cf1ea95eb347f1384ca91
RyogaIT <jdpgwa537@gmail.com>
2020-09-01 19:23:23 +0900

    userモデルのvalidation設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, present: true
+         validates :email, present: true
+         validates :password, present: true
+         validates :firstname, present: true
+         validates :lastname, present: true
+         validates :firstnamekana, present: true
+         validates :lastnamekana, present: true
+         validates :birthday, present: true
 end
