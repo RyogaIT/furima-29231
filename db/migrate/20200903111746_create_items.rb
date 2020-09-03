f4d2359d2f1a016ae7f2dac166e8903615374d3f
RyogaIT <jdpgwa537@gmail.com>
2020-09-03 20:43:26 +0900

    itemsのmigrateionfileの編集

--- db/migrate/20200903111746_create_items.rb
+++ db/migrate/20200903111746_create_items.rb
@@ -1,7 +1,17 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name,null: false, default: ""
+      t.text :detail, null: false, default: ""
+      t.integer :category, null: false, default: ""
+      t.integer :status, null: false, default: ""
+      t.integer :deliverycost, null: false, default: ""
+      t.integer :deliveryareas, null: false, default: ""
+      t.integer :deliverydates, null: false, default: ""
+      t.integer   :price, null: false, default: ""
+      t.integer   :price, null: false, default: ""
+      t.reference :user, foreign_key: true
+      t.reference :buyer, foreign_key: true
       t.timestamps
     end
   end
