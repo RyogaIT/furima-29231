e41352645410c8ff528949c08fe0ce739d051b85
RyogaIT <jdpgwa537@gmail.com>
2020-09-03 20:15:57 +0900

    itemsコントローラーの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,23 @@
 class ItemsController < ApplicationController
+  
+  def index
+  end
+
+  def new
+    @item = item.new
+  end
+
+  def create
+    @item = item.new(item_param)
+    if @item.save
+      redirect_to item_path
+    else
+      render :new
+    end
+  end
+
+  def item_param
+    params.require(:item).permit(:name, :detail, :category, :status, :deliverycost,
+    :deliveryareas, :deliverydates, :price, :user, :buyer)
+
 end
