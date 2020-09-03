643b3dc5ec0774d0340f6c45f2cdb80016486a24
RyogaIT <jdpgwa537@gmail.com>
2020-09-03 20:46:23 +0900

    item_buyersのmigarationfileの編集

--- db/migrate/20200903114507_create_item_buyers.rb
+++ db/migrate/20200903114507_create_item_buyers.rb
@@ -1,7 +1,8 @@
 class CreateItemBuyers < ActiveRecord::Migration[6.0]
   def change
     create_table :item_buyers do |t|
-
+      t.reference :user, foreign_key: true
+      t.reference :buyer, foreign_key: true
       t.timestamps
     end
   end
