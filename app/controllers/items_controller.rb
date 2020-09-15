f459bf21af5ec257526f4f921f0cf78af3f503c6
RyogaIT <jdpgwa537@gmail.com>
2020-09-15 17:34:16 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, onlu: [:show, :edit, :update]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,27 +21,29 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    if item = Item.find(params[:id])
-       item.update(item_params)
+    if
+       @item.update(item_params)
        redirect_to root_path
     else
       render :edit
     end
   end
 
-  
-
+  private
+ 
   def item_params
     params.require(:item).permit(:image, :name, :detail, :category_id, :status_id, :deliverycost_id,
     :deliveryarea_id, :deliverydate_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
