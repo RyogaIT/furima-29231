7eaf955ee17e9f1d2624764db4afde5db4d9e747
RyogaIT <jdpgwa537@gmail.com>
2020-09-15 11:36:23 +0900

    itemsコントローラーのコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,20 +22,20 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def destroy
-   item = Item.find(params[:id])
-   item.destroy
-   redirect_to root_path
-  end
+  # def destroy
+  #  item = Item.find(params[:id])
+  #  item.destroy
+  #  redirect_to root_path
+  # end
 
-  def edit
-    item = Item.find(params[:id])
-  end
+  # def edit
+  #   item = Item.find(params[:id])
+  # end
 
-  def update
-    item = Item.find(params[:id])
-    item.update.(item_param)
-  end
+  # def update
+  #   item = Item.find(params[:id])
+  #   item.update.(item_param)
+  # end
 
   def item_param
     params.require(:item).permit(:image, :name, :detail, :category_id, :status_id, :deliverycost_id,
