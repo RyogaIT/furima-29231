a1bbaf37ca1d6f07d0d5d50fdec232acb4b07daf
RyogaIT <jdpgwa537@gmail.com>
2020-09-15 22:03:31 +0900

    コンフリクトの生成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -12,7 +13,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_param)
     if @item.valid?
-      @item.save
+       @item.save
       return redirect_to root_path
     else
       render :new
@@ -20,11 +21,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    item.update.(item_param)
+  end
+
+
   def destroy
-    item = Item.find(params[:id])
     if item.valid?
     item.destroy
     redirect_to root_path
@@ -36,6 +43,12 @@ class ItemsController < ApplicationController
   def item_param
     params.require(:item).permit(:image, :name, :detail, :category_id, :status_id, :deliverycost_id,
     :deliveryarea_id, :deliverydate_id, :price).merge(user_id: current_user.id)
+  end
+
+  def 
+
+  def set_item
+    item = Item.find(params[:id])
   end
 
 end
