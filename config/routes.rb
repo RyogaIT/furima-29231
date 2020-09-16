a1a973c956ad2bd2c9fec79f265463c81a809285
RyogaIT <jdpgwa537@gmail.com>
2020-09-16 16:40:43 +0900

    ordersのcontrollerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :items
   get 'items/:id', to: 'items#fee'
+  resources :orders, only:[:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
