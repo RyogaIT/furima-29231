d7faba2f5f5d52ccd5670649b6cfff0e059fb4f3
RyogaIT <jdpgwa537@gmail.com>
2020-09-16 16:04:48 +0900

    buyer_addreeのmigrateion fileの変更

---
+++ db/migrate/20200916065956_create_buyer_addresses.rb
@@ -0,0 +1,14 @@
+class CreateBuyerAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :buyer_addresses do |t|
+      t.string :zipnumber, false, default: ""
+      t.string :prefecture, null: false
+      t.string :city, null: false
+      t.string :address, null: false
+      t.integer :buildingname, null: false
+      t.integer :phonenumber, null: false
+      t.reference :item_buyer, null: false
+      t.timestamps
+    end
+  end
+end
