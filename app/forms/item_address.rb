700841a267afaa8c9ee111be015f51293bd2b2c2
RyogaIT <jdpgwa537@gmail.com>
2020-09-28 16:28:12 +0900

    コメントアウトの削除

--- app/forms/item_address.rb
+++ app/forms/item_address.rb
@@ -8,20 +8,13 @@ class ItemAddress
     validates :deliveryarea_id, numericality: { other_than: 0}
     validates :city
     validates :address
-    # validates :buildingname
     validates :phonenumber, format: { with: /\A[0-9]+\z/, message: "is invalid. Input full-width characters."}
 end
 
-# belongs_to_active_hash :deliveryarea
-
 
 def save
-  # ユーザーの情報を保存し、「buyer_address」という変数に入れている
-  
   itembuyer = ItemBuyer.create(user_id: user_id, item_id: item_id)
   BuyerAddress.create(zipnumber: zipnumber, deliveryarea_id: deliveryarea_id, city: city, address: address, buildingname: buildingname, phonenumber: phonenumber, item_buyer_id: itembuyer.id)
-  # 商品と購入者の情報を保存
-  
 end
 
 end
