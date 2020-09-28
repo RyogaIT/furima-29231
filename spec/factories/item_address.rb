dcbd6c0a9fc5b6374ae926720535b4a11594c112
RyogaIT <jdpgwa537@gmail.com>
2020-09-28 15:02:41 +0900

    item_addressのfactory生成

---
+++ spec/factories/item_address.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :item_address do
+    zipnumber {"123-4567"}
+    deliveryarea_id {1}
+    city {"横浜"}
+    address {"青山"}
+    phonenumber {"09012345678"}
+    
+    association :user 
+    association :item
+  end
+end
--- spec/factories/item_buyers.rb
+++ spec/factories/item_buyers.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :item_buyer do
     association :user
     association :item
+
+    token :qqqqk
   end
 end
