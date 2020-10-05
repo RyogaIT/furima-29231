22d3caefba11bc100551549d0f0debc5a03d5e65
RyogaIT <jdpgwa537@gmail.com>
2020-10-05 18:24:25 +0900

    deploy.rbの編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'furima-29231'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com/RyogaIT/furima-29231/'
+set :repo_url,  'git@github.com:RyogaIT/furima-29231.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
