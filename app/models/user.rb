22750e501211c8fc0f1f0df2e2d53bb74ae51c63
RyogaIT <jdpgwa537@gmail.com>
2020-10-08 16:34:18 +0900

    passwordのvalidation変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   with_options presence: true do
          validates :nickname, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input full-width characters."}
        #   validates :email, format: {message: "is invalid. Input full-width characters."}
-         validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input full-width characters."}
+        #  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input full-width characters."}
          validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
          validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
          validates :firstnamekana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."}
